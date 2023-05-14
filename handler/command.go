2c88974956462b0abecfc8d3116c7d383777b281
Muhammad <rachman.sd@gmail.com>
2023-05-14 18:14:26 +0630

    feat: loop command registered

---
+++ handler/command.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/rachmanzz/nextjob/modules/commands"
+	"github.com/rachmanzz/nextjob/setup"
+)
+
+func HandleCommand(botMsg *tgbotapi.Message) {
+	var bot = setup.BOT
+	msg := tgbotapi.NewMessage(botMsg.Chat.ID, "")
+	for _, v := range commands.RegisteredCommand {
+		if v.CommandName == botMsg.Command() {
+			var name string = botMsg.Chat.UserName
+			if botMsg.Chat.FirstName != "" {
+				name = botMsg.Chat.FirstName
+			}
+
+			bot.Request(tgbotapi.DeleteMessageConfig{
+				MessageID: botMsg.MessageID,
+				ChatID:    botMsg.Chat.ID,
+			})
+
+			v.CommandAction(&msg, name, botMsg.Chat.ID, botMsg.MessageID, botMsg.Chat.UserName)
+
+		}
+	}
+}
