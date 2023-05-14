9493bcd35352ffeb7d7bf9c7ff7438653435bfb8
Muhammad <rachman.sd@gmail.com>
2023-05-14 18:13:59 +0630

    feat: register command handling

---
+++ modules/commands/register.go
@@ -0,0 +1,26 @@
+package commands
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/rachmanzz/nextjob/modules/commands/src"
+)
+
+type RegisteredCommandType struct {
+	CommandName   string
+	CommandAction func(msg *tgbotapi.MessageConfig, opt ...any)
+}
+
+var RegisteredCommand []RegisteredCommandType
+
+func init() {
+	RegisteredCommand = []RegisteredCommandType{
+		{
+			CommandName:   "start",
+			CommandAction: src.StartCommand,
+		},
+		{
+			CommandName:   "search",
+			CommandAction: src.SearchCommand,
+		},
+	}
+}
