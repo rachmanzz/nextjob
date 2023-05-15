f224a2f8a78a66fd6a21c7aef31ad25988d6256c
Muhammad <rachman.sd@gmail.com>
2023-05-15 13:23:21 +0630

    fix: :art: move redis setup

--- setup/botinit.go
+++ setup/botinit.go
@@ -1,33 +1,31 @@
 package setup
 
 import (
+	"log"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
-	"github.com/redis/go-redis/v9"
 )
 
 var BOT *tgbotapi.BotAPI
-var REDIS *redis.Client
 
 func init() {
-	err := godotenv.Load("local.env")
-	if err != nil {
-		panic(err)
+	if os.Getenv("LOCAL_ENV") == "yes" {
+		err := godotenv.Load("local.env")
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		log.Println("no local env")
 	}
+
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("SECRET"))
 	if err != nil {
 		panic(err)
 	}
 
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
-
 	BOT = bot
 
-	REDIS = rdb
+	RunRedis()
 }
