06260533b53391d0ec352e1318fe721f7e371154
Muhammad <rachman.sd@gmail.com>
2023-05-19 12:37:27 +0630

    feat: argument env

---
+++ setup/vararg.go
@@ -0,0 +1,79 @@
+package setup
+
+import (
+	"errors"
+	"os"
+	"strconv"
+)
+
+type VarArgType struct {
+	BotAPI      *string
+	ShowCaseAPI *string
+	RedisHost   string
+	RedisPass   string
+	RedisDB     int
+	Production  bool
+}
+
+var VarArgData *VarArgType
+
+func PopulateVarArg() error {
+	vararg := VarArgType{
+		RedisHost:  "localhost:6379",
+		RedisPass:  "",
+		RedisDB:    0,
+		Production: false,
+	}
+
+	botApi := os.Getenv("BOT_API")
+	if botApi == "" {
+		return errors.New("bot api is ampty")
+	}
+
+	vararg.BotAPI = &botApi
+
+	showCaseApi := os.Getenv("SHOWCASE_API")
+
+	if showCaseApi == "" {
+		return errors.New("showcase api is ampty")
+	}
+	vararg.ShowCaseAPI = &showCaseApi
+
+	redisHost := os.Getenv("REDIS_HOST")
+
+	// empty is OK
+	if redisHost != "" {
+		vararg.RedisHost = redisHost
+	}
+
+	redisPass := os.Getenv("REDIS_PASS")
+
+	// empty is OK
+	if redisPass != "" {
+		vararg.RedisPass = redisPass
+	}
+
+	redisDB := os.Getenv("REDIS_DB")
+
+	// empty is OK
+	if redisDB != "" {
+		db, err := strconv.Atoi(redisDB)
+		if err == nil {
+			vararg.RedisDB = db
+		}
+
+	}
+
+	strProduction := os.Getenv("PRODUCTION")
+	if strProduction != "" {
+		isProd, err := strconv.ParseBool(strProduction)
+		if err == nil && isProd {
+			vararg.Production = isProd
+		}
+
+	}
+
+	VarArgData = &vararg
+
+	return nil
+}
