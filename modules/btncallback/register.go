8e66424d83b152a48d30e09768817b8fff767516
Muhammad <rachman.sd@gmail.com>
2023-05-25 22:11:07 +0630

    feat: register button filter

---
+++ modules/btncallback/register.go
@@ -0,0 +1,40 @@
+package btncallback
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/rachmanzz/nextjob/modules/btncallback/src/filter"
+)
+
+type RegisteredQueryType struct {
+	Query          string
+	CallbackAction func(msg tgbotapi.Message)
+}
+
+var RegisteredQuery []RegisteredQueryType
+
+func init() {
+	RegisteredQuery = []RegisteredQueryType{
+		// filter query
+		{
+			Query:          "--filter-location",
+			CallbackAction: filter.FilterLocation,
+		},
+		{
+			Query:          "--filter-type",
+			CallbackAction: filter.FilterType,
+		},
+		{
+			Query:          "--filter-currency",
+			CallbackAction: filter.FilterCurrency,
+		},
+		{
+			Query:          "--filter-salary",
+			CallbackAction: filter.FilterSalary,
+		},
+		{
+			Query:          "--filter-inline-arg",
+			CallbackAction: filter.FilterInlineArgument,
+		},
+		// end filter query
+	}
+}
