0815df746a0d91bb75c18fb85c49b38661979d93
Muhammad <rachman.sd@gmail.com>
2023-05-25 22:04:28 +0630

    fix: :art: split to proccess the button

--- handler/btncallback.go
+++ handler/btncallback.go
@@ -2,19 +2,27 @@ package handler
 
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/rachmanzz/nextjob/modules/btncallback"
 	"github.com/rachmanzz/nextjob/setup"
 )
 
 func HandleCallbackQuery(update tgbotapi.Update) {
 	var bot = setup.BOT
+
+	bot.Request(tgbotapi.DeleteMessageConfig{
+		MessageID: update.CallbackQuery.Message.MessageID,
+		ChatID:    update.CallbackQuery.Message.Chat.ID,
+	})
+
 	callback := tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)
 	if _, err := bot.Request(callback); err != nil {
 		panic(err)
 	}
 
-	msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Data)
-	if _, err := bot.Send(msg); err != nil {
-		panic(err)
+	for _, v := range btncallback.RegisteredQuery {
+		if callback.Text == v.Query {
+			v.CallbackAction(*update.CallbackQuery.Message)
+		}
 	}
 
 }
