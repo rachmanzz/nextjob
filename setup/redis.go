5920d6d2060de26c5c240b698f3d8c6b23c44412
Muhammad <rachman.sd@gmail.com>
2023-05-26 14:44:33 +0630

    update check connection

--- setup/redis.go
+++ setup/redis.go
@@ -1,10 +1,15 @@
 package setup
 
 import (
+	"context"
+	"time"
+
 	"github.com/redis/go-redis/v9"
 )
 
 var REDIS *redis.Client
+
+var ctx = context.Background()
 
 func RunRedis() {
 	rdb := redis.NewClient(&redis.Options{
@@ -12,6 +17,12 @@ func RunRedis() {
 		Password: VarArgData.RedisPass,
 		DB:       VarArgData.RedisDB,
 	})
+
+	startAt := time.Now()
+	err := rdb.Set(ctx, "nextjob_up", startAt.String(), 0).Err()
+	if err != nil {
+		panic("not connect to redis server")
+	}
 
 	REDIS = rdb
 }
