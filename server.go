b97f297509fd8778c64c099cc025ea9a905329eb
Muhammad <rachman.sd@gmail.com>
2023-05-26 23:20:59 +0630

    remove comment

--- server.go
+++ server.go
@@ -12,7 +12,6 @@ import (
 
 func main() {
 	var bot = setup.BOT
-	//var redis = setup.REDIS
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
@@ -20,38 +19,11 @@ func main() {
 	u.Timeout = 60
 	updates := bot.GetUpdatesChan(u)
 
-	// var numericKeyboard = tgbotapi.NewReplyKeyboard(
-	// 	tgbotapi.NewKeyboardButtonRow(
-	// 		tgbotapi.NewKeyboardButton("filter"),
-	// 		tgbotapi.NewKeyboardButton("2"),
-	// 		tgbotapi.NewKeyboardButton("3"),
-	// 	),
-	// 	tgbotapi.NewKeyboardButtonRow(
-	// 		tgbotapi.NewKeyboardButton("4"),
-	// 		tgbotapi.NewKeyboardButton("5"),
-	// 		tgbotapi.NewKeyboardButton("6"),
-	// 	),
-	// )
-
-	// var filterButton = tgbotapi.NewInlineKeyboardMarkup(
-	// 	tgbotapi.NewInlineKeyboardRow(
-	// 		tgbotapi.NewInlineKeyboardRowButtonData("location", "--filter-location"),
-	// 		tgbotapi.NewInlineKeyboardButtonData("salary start", "--filter-salary-from"),
-	// 		tgbotapi.NewInlineKeyboardButtonData("salary until", "--filter-salary-end"),
-	// 	),
-	// 	tgbotapi.NewInlineKeyboardRow(
-	// 		tgbotapi.NewInlineKeyboardButtonData("currency", "--filter-currency"),
-	// 		tgbotapi.NewInlineKeyboardButtonData("experience", "--filter-experience"),
-	// 		tgbotapi.NewInlineKeyboardButtonData("next", "--filter-next"),
-	// 	),
-	// )
-
 	debounced := debounce.New(2000 * time.Millisecond)
 
 	for update := range updates {
-		if update.Message != nil { // If we got a message
-			//msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-			if update.Message.IsCommand() { // ignore any non-command Messages
+		if update.Message != nil {
+			if update.Message.IsCommand() {
 				handler.HandleCommand(update.Message)
 			} else {
 				handler.HandleInput(update.Message)
@@ -63,20 +35,6 @@ func main() {
 
 			if update.InlineQuery != nil {
 				debounced(func() { handler.HandleInlieQuery(update.InlineQuery) })
-
-				// article := tgbotapi.NewInlineQueryResultArticle(update.InlineQuery.ID, "Echo", update.InlineQuery.Query)
-				// article.Description = update.InlineQuery.Query
-
-				// inlineConf := tgbotapi.InlineConfig{
-				// 	InlineQueryID: update.InlineQuery.ID,
-				// 	IsPersonal:    true,
-				// 	CacheTime:     0,
-				// 	Results:       []interface{}{article},
-				// }
-
-				// if _, err := bot.Request(inlineConf); err != nil {
-				// 	log.Println(err)
-				// }
 			}
 		}
 	}
